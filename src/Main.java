7169bf7d51451d6064163483581b6adcdc895e2a
raghu0056 <raghumani0056@gmail.com>
2023-03-13 23:52:22 +0530

    Using switch case statment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 class Employee
         {
-          int Hours=8;
+            int Hours=8;
             String p="Present";
             String a="Absent";
             int Wage=20;
@@ -18,15 +18,43 @@ class Employee
         int WorkingHours = sc.nextInt();
         Employee ob=new Employee();
         int TotalWages=WorkingHours * ob.Wage;
-        if(WorkingHours > ob.Hours)
-        {
-            System.out.println("Employee total wages for per day is " +TotalWages);
-            System.out.println( "Employee working in full time and " +ob.p);
+        switch(WorkingHours){
+            case 1:
+                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.p);
+                break;
 
-         }
-        else {
-            System.out.println("Employee total wages for per day is" +TotalWages);
-            System.out.println( "Employee working in Part time and " +ob.a);
+            case 2:
+                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.p);
+                break;
+
+            case 3:
+                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.p);
+                break;
+            case 4:
+                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.a);
+                break;
+            case 5:
+                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.p);
+                break;
+            case 6:
+                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.a);
+                break;
+            case 7:
+                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.p);
+                break;
+            case 8:
+                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.a);
+                break;
+                default:
+                System.out.println("Please check the input");
 
         }
     }
