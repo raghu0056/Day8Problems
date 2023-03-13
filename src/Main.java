a54f0ce467475a200566ca76340e46963944744d
raghu0056 <raghumani0056@gmail.com>
2023-03-13 22:50:15 +0530

    Employee present or absent program

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
-public class Main
+
+import java.util.Random;
+class Employee
+        {
+            String p="Present";
+           String a="Absent";
+        }
+ public class Main
 {
-    String Emp="Welcome to Employee wage computation";
     public static void main(String[] args)
     {
-        Main ob=new Main();
-        System.out.println(ob.Emp);
+        Random rand = new Random();
+        int value = rand.nextInt(100);
+        Employee ob=new Employee();
+        if(value < 50)
+     {
+         System.out.println("Employee is " +ob.p);
+     }
+     else
+     {
+         System.out.println("Employee is " +ob.a);
+     }
     }
 }
