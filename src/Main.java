2f5e01c7b6807a1e8c6c5b1843664af4b451a2c5
raghu0056 <raghumani0056@gmail.com>
2023-03-13 22:11:50 +0530

    Employee wage computation

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    String Emp="Welcome to Employee wage computation";
+    public static void main(String[] args)
+    {
+        Main ob=new Main();
+        System.out.println(ob.Emp);
+    }
+}
