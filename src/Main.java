e26ea062cb9720624116cada4824458d1dc4d5c8
raghu0056 <raghumani0056@gmail.com>
2023-03-14 22:19:00 +0530

    Employee wages for month

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ class Employee
             int Hours=8;
             String p="Present";
             String a="Absent";
+             int month=20;
             int Wage=20;
         }
  public class Main
@@ -18,40 +19,49 @@ class Employee
         int WorkingHours = sc.nextInt();
         Employee ob=new Employee();
         int TotalWages=WorkingHours * ob.Wage;
+        int TotalMonth=TotalWages* ob.month;
         switch(WorkingHours){
             case 1:
-                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println("Employee total wages for per day is:" +TotalWages);
                 System.out.println( "Employee working in full time and " +ob.p);
+                System.out.println( "Per month salary of the employee is: " +TotalMonth);
+
                 break;
 
             case 2:
-                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println("Employee total wages for per day is:" +TotalWages);
                 System.out.println( "Employee working in full time and " +ob.p);
+                System.out.println( "Per month salary of the employee is: " +TotalMonth);
                 break;
-
             case 3:
-                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println("Employee total wages for per day is:" +TotalWages);
                 System.out.println( "Employee working in full time and " +ob.p);
+                System.out.println( "Per month salary of the employee is: " +TotalMonth);
                 break;
             case 4:
-                System.out.println("Employee total wages for per day is " +TotalWages);
-                System.out.println( "Employee working in full time and " +ob.a);
+                System.out.println("Employee total wages for per day is:" +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.p);
+                System.out.println( "Per month salary of the employee is: " +TotalMonth);
                 break;
             case 5:
-                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println("Employee total wages for per day is:" +TotalWages);
                 System.out.println( "Employee working in full time and " +ob.p);
+                System.out.println( "Per month salary of the employee is: " +TotalMonth);
                 break;
             case 6:
-                System.out.println("Employee total wages for per day is " +TotalWages);
-                System.out.println( "Employee working in full time and " +ob.a);
+                System.out.println("Employee total wages for per day is:" +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.p);
+                System.out.println( "Per month salary of the employee is: " +TotalMonth);
                 break;
             case 7:
-                System.out.println("Employee total wages for per day is " +TotalWages);
+                System.out.println("Employee total wages for per day is:" +TotalWages);
                 System.out.println( "Employee working in full time and " +ob.p);
+                System.out.println( "Per month salary of the employee is: " +TotalMonth);
                 break;
             case 8:
-                System.out.println("Employee total wages for per day is " +TotalWages);
-                System.out.println( "Employee working in full time and " +ob.a);
+                System.out.println("Employee total wages for per day is:" +TotalWages);
+                System.out.println( "Employee working in full time and " +ob.p);
+                System.out.println( "Per month salary of the employee is: " +TotalMonth);
                 break;
                 default:
                 System.out.println("Please check the input");
